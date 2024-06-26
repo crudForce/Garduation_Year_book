8e6516165509c971f1d51a0e12e1c28dd251f1e5
idi8there <eyob01tesfaye@gmail.com>
2024-06-26 07:27:02 -0700

    corrected the supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,6 +4,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://exttgrmtjbijllepzsxv.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4dHRncm10amJpamxsZXB6c3h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0MzY4MDMsImV4cCI6MjAzNDAxMjgwM30.-m-nif2yJmggOVd-HgPT2AJJJIo5-etkbVW3j57KfFk";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const Supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+export default Supabase;
